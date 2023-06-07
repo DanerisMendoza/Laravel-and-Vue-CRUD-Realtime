5a6d0a6d6ab3ecac942e266003a9d8082f68b9b5
Daneris <danerismendoza096@gmail.com>
2023-06-07 12:11:36 +0800

    been able to console log the websocket broadcast function

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Hello Vue js</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+
+  mounted(){
+    window.Echo.channel('channel').listen('Hello',(e) => {
+      console.log(e);
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import Echo from 'laravel-echo';
+
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.VUE_APP_WEBSOCKETS_KEY,
+    wsHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+    cluster: 'mt1'
+});
+
+createApp(App).mount('#app')
